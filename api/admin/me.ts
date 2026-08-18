3435e8bccf129a4c9de6fd75d7c951e07210ebea
radhakr269 <radhakr269@gmail.com>
2026-08-18 23:21:30 +0530

    Update me.ts
    Fix me.ts auth import

--- api/admin/me.ts
+++ api/admin/me.ts
@@ -1,4 +1,4 @@
-import { extractAdminSession } from '../../server/auth';
+import { extractAdminSession } from '../../server/auth.js';
 
 export default async function handler(req: any, res: any) {
   res.setHeader('Content-Type', 'application/json');
